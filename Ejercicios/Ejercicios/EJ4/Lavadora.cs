93134859bda78309cbecc188b2419566f4d22bbc
JoanJuan10 <iijoanjuan10@gmail.com>
2021-01-07 09:29:01 +0100

    Ejercicio 4

---
+++ Ejercicios/Ejercicios/EJ4/Lavadora.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicios.EJ4
+{
+    class Lavadora : Electrodomestico
+    {
+        private const int cargaDefault = 5;
+        private int carga = cargaDefault;
+
+        public int Carga { get => carga;}
+
+        public Lavadora(double precioBase, string color, char consumo, double peso, int carga) : base (precioBase, color, consumo, peso)        
+        {
+            this.carga = carga;
+        }
+        public Lavadora(double precioBase, double peso) : base(precioBase, peso)
+        {
+        }
+        public Lavadora() : base()
+        {
+
+        }
+        public new void precioFinal () 
+        {
+            base.precioFinal();
+            if (this.carga > 30)
+            {
+                this.PrecioBase += 50;
+            }
+        }
+
+    }
+}
