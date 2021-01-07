76a84588943944d60b55cec675882d45a7f05dc8
JoanJuan10 <iijoanjuan10@gmail.com>
2021-01-07 11:19:39 +0100

    Ejercicio 7

---
+++ Ejercicios/Ejercicios/EJ7/Raices.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicios.EJ7
+{
+    class Raices
+    {
+        private int a;
+        private int b;
+        private int c;
+        private double sol1;
+        private double sol2;
+        private bool unicaSolucion;
+
+        public Raices(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public void ObtenerRaices ()
+        {
+            Console.WriteLine("Solucion 1: {0}", this.sol1);
+            Console.WriteLine("Solucion 2: {1}", this.sol2);
+        }
+        public void ObtenerRaiz()
+        {
+            if (unicaSolucion) 
+            {
+                Console.WriteLine("Solucion 1: {0}", this.sol1);
+            }
+            else
+            {
+                ObtenerRaices();
+            }
+        }
+        public double getDiscriminante()
+        {
+            return Math.Pow(this.b, 2) - (4 * this.a * this.c);
+        }
+        public bool TieneRaices()
+        {
+            return (getDiscriminante() >= 0) ? true : false;
+        }
+        public bool TieneRaiz()
+        {
+            return (getDiscriminante() == 0) ? true : false;
+        }
+        public void Calcular()
+        {
+            if (getDiscriminante() < 0)
+            {
+                Console.WriteLine("No tiene solucion");
+            }
+            else if (getDiscriminante() == 0)
+            {
+                unicaSolucion = true;
+                this.sol1 = (-1 * this.b) / (2 * this.a);
+            }
+            else
+            {
+                unicaSolucion = false;
+                this.sol1 = ((-1 * this.b) + Math.Sqrt(getDiscriminante())) / (2 * this.a);
+                this.sol2 = ((-1 * this.b) - Math.Sqrt(getDiscriminante())) / (2 * this.a);
+            }
+             
+        }
+    }
+}
